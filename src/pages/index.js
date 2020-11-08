6475e0aa8d76b44b9daf8916c66403f8ba10e2cc
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2020-11-08 21:54:14 +0100

    remove unused import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Button, Container, Jumbotron } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import styles from "./index.module.css"
 
 export default function Home() {
   return (
