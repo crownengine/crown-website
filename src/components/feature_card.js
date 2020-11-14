890dbad950c6f02b043ae24c615f1a20dc2cf1f9
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2020-11-14 10:24:57 +0100

    remove fade

--- src/components/feature_card.js
+++ src/components/feature_card.js
@@ -1,27 +1,13 @@
 import React from "react"
-import { Card, Fade } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 
 const FeatureCard = ({ title, text }) => {
-  const [isShown, setIsShown] = React.useState(false)
-
-  const show = () => {
-    setIsShown(true)
-  }
-
-  const hide = () => {
-    setIsShown(false)
-  }
-
   return (
     <>
-      <Card border="dark">
+      <Card border="dark" className={"h-100"}>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
-          <Fade appear={false} in={isShown}>
-            <Card.Text onMouseEnter={show} onMouseLeave={hide}>
-              {text}
-            </Card.Text>
-          </Fade>
+          <Card.Text>{text}</Card.Text>
         </Card.Body>
       </Card>
     </>
