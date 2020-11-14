8b7e4234505df7dcd5f9d1a43e552c13b767ca7a
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2020-11-14 08:29:41 +0100

    sticky footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,13 @@ import React from "react"
 import { Navbar } from "react-bootstrap"
 
 const Footer = () => (
-  <Navbar bg="dark" variant="light" fixed="bottom">
-    <Navbar.Text className="text-light">
-      The Crown Foundation © {new Date().getFullYear()}
-    </Navbar.Text>
-  </Navbar>
+  <footer>
+    <Navbar bg="dark" variant="light" sticky="bottom">
+      <Navbar.Text className="text-light">
+        The Crown Foundation © {new Date().getFullYear()}
+      </Navbar.Text>
+    </Navbar>
+  </footer>
 )
 
 export default Footer
