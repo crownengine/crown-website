e7f264623ff4b402e61d31f4facaa899c24c2c31
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2022-02-05 16:41:32 +0100

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
