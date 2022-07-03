e777949702a6a2135805c7696d4dfc7b909ee5e6
Daniele Bartolini <dbartolini.aa@gmail.com>
2022-07-03 19:42:39 +0200

    slightly decrease overall contrast

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
       <Helmet>
         <link rel="icon" type="image/png" href={favicon} />
       </Helmet>
-      <div className="dark:text-white">
+      <div className="dark:text-white bg-gray-100 dark:bg-gray-800">
         <div className="flex flex-col min-h-screen">
           <Header />
           <div className="container center mx-auto lg:w-5/11 flex flex-col">
