3c1eae52ee040ce402f4c5b92459386d858ff423
Daniele Bartolini <dbartolini.aa@gmail.com>
2022-08-09 00:32:55 +0200

    donate: approved for GitHub Sponsors

--- src/pages/fund.jsx
+++ src/pages/fund.jsx
@@ -13,68 +13,70 @@ export default function Fund() {
           Donations
         </h2>
 
-        {/* GitHub Sponsors */}
         <div className="flex flex-col gap-6">
-        {/*
-          <div className="rounded-lg shadow-lg p-6 bg-github-pink text-gray-700">
-            <h2 className="text-3xl mb-2 font-semibold underline">
-              <a href="https://github.com/sponsors/crownengine?preview=true">
-                GitHub Sponsors
+          <div className="flex flex-col sm:flex-row gap-6">
+            {/* GitHub Sponsors */}
+            <div className="flex-1 rounded-lg shadow-lg p-6 bg-github-pink text-gray-800">
+              <h2 className="text-3xl mb-2 font-semibold">
+                <a href="https://github.com/sponsors/crownengine?preview=true">
+                  GitHub Sponsors
+                </a>
+              </h2>
+              <p className="mb-6">
+                This is the easiest way to support us if your already have a GitHub account.
+              </p>
+              <a
+                className="
+                    block
+                    sm:inline-block
+                       mt-4
+                    sm:mt-0
+
+                         hover:text-white
+                    dark:hover:bg-indigo-400
+                         text-white
+                    dark:text-black
+
+                          bg-indigo-600
+                     dark:bg-indigo-500
+                    hover:bg-indigo-700
+
+                    rounded
+                    shadow
+                    px-4
+                    py-2
+                    w-full
+                    text-center
+                  "
+                href="https://github.com/sponsors/crownengine?preview=true"
+              >
+                Become a sponsor
               </a>
-            </h2>
-            <p className="mb-6">
-              GitHub Sponsors is the easiest way to support us if your already have a GitHub account.
-            </p>
-            <a
-              className="
-                  block
-                  sm:inline-block
-                     mt-4
-                  sm:mt-0
+            </div>
 
-                       hover:text-white
-                  dark:hover:bg-indigo-400
-                       text-white
-                  dark:text-black
+            {/* PayPal */}
+            <div className="flex-1 rounded-lg shadow-lg p-6 bg-paypal-blue text-gray-200">
+              <h2 className="text-3xl mb-2 font-semibold">
+                PayPal
+              </h2>
+              <div className="flex flex-col sm:flex-row">
+                <div className="flex-1 mb-6">
+                  <p>
+                    The Crown Foundation accepts donations via PayPal.
+                    <br/>
+                    Pay with debit or credit cards.
+                  </p>
+                </div>
 
-                        bg-indigo-600
-                   dark:bg-indigo-500
-                  hover:bg-indigo-700
-
-                  rounded
-                  shadow
-                  px-4
-                  py-2
-                "
-              href="https://github.com/sponsors/crownengine?preview=true"
-            >
-              Join Crown's GitHub Sponsors
-            </a>
-          </div>
-        */}
-
-          {/* PayPal */}
-          <div className="flex-1 rounded-lg shadow-lg p-6 bg-paypal-blue text-gray-200">
-            <h2 className="text-3xl mb-2 font-semibold">
-              PayPal
-            </h2>
-            <div className="flex flex-col sm:flex-row">
-              <div className="flex-1 mb-6">
-                <p>
-                  The Crown Foundation accepts donations via PayPal.
-                  <br/>
-                  Pay with debit or credit cards.
-                </p>
-              </div>
-
-              <div className="flex-1">
-                <div className="flex flex-col sm:flex-row gap-6 place-items-center">
-                  <div className="flex-1 mt-auto mb-auto">
-                    <form action="https://www.paypal.com/donate" method="post" target="_top">
-                      <input type="hidden" name="hosted_button_id" value="2D9YQLJNH39V6" />
-                      <input type="image" src="https://www.paypalobjects.com/en_US/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                      <img alt="" border="0" src="https://www.paypal.com/en_IT/i/scr/pixel.gif" width="1" height="1" />
-                    </form>
+                <div className="flex-1">
+                  <div className="flex flex-col sm:flex-row gap-6 place-items-center">
+                    <div className="flex-1 mt-auto mb-auto">
+                      <form action="https://www.paypal.com/donate" method="post" target="_top">
+                        <input type="hidden" name="hosted_button_id" value="2D9YQLJNH39V6" />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                        <img alt="" border="0" src="https://www.paypal.com/en_IT/i/scr/pixel.gif" width="1" height="1" />
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -82,7 +84,7 @@ export default function Fund() {
           </div>
 
           {/* Bank Transfer */}
-          <div className="rounded-lg shadow-lg p-6 bg-gray-500 text-gray-200">
+          <div className="flex-1 rounded-lg shadow-lg p-6 bg-gray-500 text-gray-200">
             <h2 className="text-3xl mb-2 font-semibold">
               Bank Transfer
             </h2>
