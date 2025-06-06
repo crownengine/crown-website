6fd359b2e584364b8b6075687676c3a51a518be9
Daniele Bartolini <dbartolini@crownengine.org>
2025-06-07 01:30:46 +0200

    download: fix thanks page

--- src/pages/download/thanks.jsx
+++ src/pages/download/thanks.jsx
@@ -17,30 +17,30 @@ export default function Thanks({location}) {
 
   return (
     <Layout>
-      <div className="bg-gradient-to-b from-gray-900 to-gray-800">
+      <div className="pb-80 md:pb-0 bg-gradient-to-b from-gray-900 to-gray-800">
         <Clamp>
-          <section className="py-24 px-4 text-center">
+          <section className="py-0 md:py-24 px-4 text-center">
             <div className="w-full max-w-2xl mx-auto">
               <p className="mt-48 text-lg text-gray-300">
                 Your download will start automatically. If it didn't, click here to start it <a className="underline" href={download_url}>manually</a>.
               </p>
             </div>
           </section>
-          <section className="pb-64 px-4 bg-gradient-to-r from-orange-300 to-orange-600 bg-clip-text">
-            <div className="grid grid-cols-1 md:grid-cols-6 gap-6 mb-10">
-
-              <div className="grid col-span-1 md:col-span-3 gap-6">
+        </Clamp>
+        <Clamp>
+          <section className="py-24 px-4 bg-gradient-to-r from-orange-300 to-orange-600 bg-clip-text">
+            <div className="grid md:grid-cols-6 gap-6 mb-40">
+              <div className="col-span-6 md:col-span-3 flex flex-col gap-6">
                 <h2 className="text-6xl font-bold text-transparent">
-                  Every Donation Matters
+                  Help Crown grow Stronger and Faster
                 </h2>
                 <p className="text-2xl text-gray-300">
                   Donations support Core Contributors working on Features, Maintenance and Improvements &#x1F970;
                 </p>
               </div>
 
-              <div className="grid col-span-1 md:col-span-3 gap-6">
-                <DonationBox>
-                </DonationBox>
+              <div className="col-span-6 md:col-span-3 flex flex-col gap-6 relative" id="donation-box">
+                <DonationBox/>
               </div>
             </div>
           </section>
