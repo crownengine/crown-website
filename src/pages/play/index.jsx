86773c85fbb24105cdc633d40327baf4da63547a
Daniele Bartolini <dbartolini@crownengine.org>
2025-12-01 09:03:11 +0100

    play: better heading

--- src/pages/play/index.jsx
+++ src/pages/play/index.jsx
@@ -10,7 +10,9 @@ export default function Play() {
       <div className="bg-[url('../images/index/luna-background.svg')] bg-left-top bg-cover">
         <Clamp>
           <section className="py-24 px-4 text-left text-red-600">
-            <h1 className="text-6xl mb-6 font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent drop-shadow-md">Play on the web</h1>
+            <h1 className="text-6xl mb-6 font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent drop-shadow-md">
+              Take a Bite of The Tech
+            </h1>
             <p className="text-2xl mb-24 text-gray-300">
               Play a selection of interactive demos in-browser.
             </p>
@@ -28,6 +30,5 @@ export default function Play() {
 }
 
 export const Head = () => (
-  <Seo title="Play" />
+  <Seo title="Play" description="Play a selection of interactive demos in-browser."/>
 )
-
