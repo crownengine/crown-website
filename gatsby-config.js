b3c238528bed3bbe428da5d621886c8b22792af5
Daniele Bartolini <dbartolini@crownengine.org>
2025-12-30 15:30:09 +0100

    *: better remark image quality

--- gatsby-config.js
+++ gatsby-config.js
@@ -203,6 +203,8 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1200,
+              quality: 90,
+              withWebp: { quality: 90 },
             },
           },
         ],
