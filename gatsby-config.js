d8609b8a24919f60f277ba23bddd8e912d0d56ed
Daniele Bartolini <dbartolini@crownengine.org>
2026-01-04 16:56:28 +0100

    rss: use excerpt

--- gatsby-config.js
+++ gatsby-config.js
@@ -158,7 +158,7 @@ module.exports = {
             serialize: ({ query: { site, allMdx } }) => {
               return allMdx.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.body,
+                  description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
@@ -176,7 +176,7 @@ module.exports = {
                         title
                         date
                       }
-                      body
+                      excerpt
                     }
                   }
                 }
