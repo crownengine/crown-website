536b2e55032ff2b3b6cdaaf7f106fa38560d1c10
Daniele Bartolini <dbartolini@crownengine.org>
2026-08-05 19:53:33 +0200

    components: configurable download button text

--- src/components/download-button.jsx
+++ src/components/download-button.jsx
@@ -1,13 +1,13 @@
 import { Link } from "gatsby"
 import React from "react"
 
-const DownloadButton = () => {
+const DownloadButton = ({ children = "Download Crown" }) => {
   return (
     <Link
-      className="inline-block py-4 px-8 mb-2 font-semibold leading-none text-gray-200 hover:text-white bg-indigo-600 hover:bg-indigo-700 rounded shadow"
+      className="inline-block py-2 px-8 font-semibold text-gray-200 hover:text-white bg-indigo-600 hover:bg-indigo-700 rounded shadow"
       to="/download"
     >
-      Download Crown
+      {children}
     </Link>
   )
 }
